eece28aec77208c90d68e4ac874757cf1d610b2a
RyanHTL <2388092952@qq.com>
2019-11-14 13:25:08 +0800

    change test

--- src/com/mybatis/test/TestSsm.java
+++ src/com/mybatis/test/TestSsm.java
@@ -8,34 +8,35 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.util.List;
 
-public class test{
-    public static void main(String[] args) {
-        ApplicationContext context = new ClassPathXmlApplicationContext("classpath:applicationContext.xml");
-
-        UserService  u = (UserService) context.getBean("userService");
-
-        List<UserInfo> userInfoList = u.findUserinfoByUsernameAndPassword(new UserInfo("q","q","男"));
-        for (UserInfo user: userInfoList
-        ) {
-            System.out.println(user.toString());
-
-        }
-    }
-//    @Test
-//    public test() {
+public class TestSsm{
+//    public static void main(String[] args) {
 //        ApplicationContext context = new ClassPathXmlApplicationContext("classpath:applicationContext.xml");
 //
 //        UserService  u = (UserService) context.getBean("userService");
 //
 //        List<UserInfo> userInfoList = u.findUserinfoByUsernameAndPassword(new UserInfo("q","q","男"));
 //        for (UserInfo user: userInfoList
-//             ) {
+//        ) {
 //            System.out.println(user.toString());
 //
 //        }
-//
-////        u.addUser(new UserInfo("q","q","女"));
-//
-//
 //    }
+
+    @Test
+    public void tests() {
+        ApplicationContext context = new ClassPathXmlApplicationContext("classpath:applicationContext.xml");
+
+        UserService  u = (UserService) context.getBean("userService");
+
+        List<UserInfo> userInfoList = u.findUserinfoByUsernameAndPassword(new UserInfo("q","q","男"));
+        for (UserInfo user: userInfoList
+             ) {
+            System.out.println(user.toString());
+
+        }
+
+//        u.addUser(new UserInfo("q","q","女"));
+
+
+    }
 }
